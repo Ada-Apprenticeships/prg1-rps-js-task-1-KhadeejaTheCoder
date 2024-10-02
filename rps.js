94453d63d9d77094cbe3cee397d763c2935b5191
KhadeejaTheCoder <khadeeja.ahmed@ada.ac.uk>
2024-10-02 08:40:35 +0000

    passes both draw tests

--- rps.js
+++ rps.js
@@ -1,19 +1,22 @@
 function rockPaperScissors(player1, player2) {
-
-  
-   
+  if ((player1 = player2)) {
+    return "draw";
+  } else if (player1 === "rock" && player2 === "scissors") {
+    return "player1";
+  } else if (player1 === "rock" && player2 === "paper") {
+    return "player2";1
+  } else if (player1 === "paper" && player2 === "rock") {
+    return "player1";
+  } else if (player1 === "paper" && player2 === "scissors") {
+    return "player2";
+  } else if (player1 === "scissors" && player2 === "rock") {
+    return "player2";
+  } else if (player1 === "scissors" && player2 === "paper") {
+    return "player1";
+  }
 }
-
-
-
-
-
-
-
-
-
 
 // Leave this code here for the automated tests
 module.exports = {
   rockPaperScissors,
-}
+};
