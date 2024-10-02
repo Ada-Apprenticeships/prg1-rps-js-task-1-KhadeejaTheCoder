46aea491f7d4176734df2dd0ceb1b132219c2f19
KhadeejaTheCoder <khadeeja.ahmed@ada.ac.uk>
2024-10-02 08:51:28 +0000

    passes first 4 tests

--- rps.js
+++ rps.js
@@ -1,10 +1,10 @@
 function rockPaperScissors(player1, player2) {
-  if ((player1 = player2)) {
+  if ((player1 === player2)) {
     return "draw";
   } else if (player1 === "rock" && player2 === "scissors") {
     return "player1";
   } else if (player1 === "rock" && player2 === "paper") {
-    return "player2";1
+    return "player2";
   } else if (player1 === "paper" && player2 === "rock") {
     return "player1";
   } else if (player1 === "paper" && player2 === "scissors") {
